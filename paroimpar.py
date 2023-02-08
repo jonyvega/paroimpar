006f2a874dbb718d099a06979bdc8b14ff7fdb3d
jonyvega <godopportunityspirit@gmail.com>
2023-02-08 01:03:48 +0100

    proyecto resuelto para ver si es par o impar

--- paroimpar.py
+++ paroimpar.py
@@ -1,1 +1,9 @@
-numero = input("Que numero estas pensando?")
+numero = int(input("Que numero estas pensando?"))
+resultado = numero % 2
+while resultado != 0:
+    if resultado != 0:
+        print("Es impar, dale con otro: ")
+        numero = int(input("Que--numero estas pensando?"))
+        resultado = numero % 2       
+print("Es par")
+ 
